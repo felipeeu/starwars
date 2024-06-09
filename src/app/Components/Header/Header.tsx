e76255e2f55b61ff1eff789d5fa974368e583053
Felipe Domingues <felipeeu@yahoo.com.br>
2024-06-09 14:50:11 -0300

    fix: last adjustments

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -7,9 +7,9 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title, body }) => {
   return (
-    <div className="px-7 flex flex-col font-helvetica-neue gap-2.5 lg:gap-[25px]">
-      <h1 className="lg:text-[54px] lg:leading-[64px] capitalize">{title}</h1>
-      <p className="p-header lg:w-[960px] lg:text-[22px] lg:leading-8 lg:pr-[26px]">
+    <div className="px-7 flex flex-col font-helvetica-neue gap-2.5 md:gap-[25px]">
+      <h1 className="md:text-[54px] md:leading-[64px] capitalize">{title}</h1>
+      <p className="p-header md:w-[960px] md:text-[22px] md:leading-8 md:pr-[26px]">
         {body}
       </p>
     </div>
--- src/app/constants.ts
+++ src/app/constants.ts
@@ -4,6 +4,6 @@ export const COLUMNS_QUANTITY = 4;
 export const NONE_VISIBLE_ITEMS = 0;
 export const HEADER_TITLE = "star wars characters";
 export const HEADER_BODY =
-  "This web app was created by Felipe Domingues in order to fullfill a challenge.";
+  "This web app was created by Felipe Domingues in order to fulfill a challenge.";
 export const FILTER_COMPONENT_LABEL = "filter by";
 export const LIST_ITEM_TITLE = "all characters";
