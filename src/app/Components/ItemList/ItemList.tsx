f35481252ebbbc151145bdb1a1ac352dd50490ec
Felipe Domingues <felipeeu@yahoo.com.br>
2024-06-09 09:01:58 -0300

    feat: button works with data population

--- src/app/Components/ItemList/ItemList.tsx
+++ src/app/Components/ItemList/ItemList.tsx
@@ -1,18 +1,22 @@
 "use client";
 import useDataRequest, { Person } from "@/app/hooks/useDataRequest";
 import { ResponsiveItem } from "../ItemCard";
-import { useEffect, useState } from "react";
+import { MouseEventHandler } from "react";
 
 interface ItemListProps {
   title: string;
   selectedPlanet: string;
   defaultValue: string;
+  visibleItems: number;
+  onClick: MouseEventHandler<HTMLButtonElement>;
 }
 
 const ItemList: React.FC<ItemListProps> = ({
   title,
   selectedPlanet,
   defaultValue,
+  visibleItems,
+  onClick,
 }) => {
   const { people, loading, error } = useDataRequest();
   const isDefaultValue = selectedPlanet === defaultValue;
@@ -26,7 +30,7 @@ const ItemList: React.FC<ItemListProps> = ({
   }
   return (
     <div className="flex flex-col w-auto px-7 font-helvetica-neue gap-[2.5em]">
-      <h2 className="font-light text-[#333333] leading-10 lg:text-34">
+      <h2 className="font-light text-[#333333] leading-10 lg:text-34 capitalize">
         {title}
       </h2>
       <div className="lg:grid lg:grid-cols-4">
@@ -34,13 +38,17 @@ const ItemList: React.FC<ItemListProps> = ({
           .filter((person: Person) =>
             isDefaultValue ? person : person.homeworld === selectedPlanet
           )
+          .slice(0, visibleItems)
           .map((person: Person) => (
             <div key={person.name}>
               <ResponsiveItem person={person} />
             </div>
           ))}
       </div>
-      <button className="min-w-[196px] border border-[#002B53] self-center lg:min-w-[496px]">
+      <button
+        className="min-w-[196px] border border-[#002B53] self-center lg:min-w-[496px]"
+        onClick={onClick}
+      >
         <span className="loadmore uppercase text-[#002B53]">load more</span>
       </button>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,50 @@
 "use client";
-import { useCallback, useState } from "react";
-import { FilterComponent, ItemList } from "./Components";
+import { ChangeEvent, useCallback, useState } from "react";
+import { FilterComponent, Header, ItemList } from "./Components";
+import {
+  DEFAULT_VALUE,
+  DEFAULT_VISIBLE_ITEMS,
+  COLUMNS_QUANTITY,
+  NONE_VISIBLE_ITEMS,
+  HEADER_TITLE,
+  HEADER_BODY,
+  FILTER_COMPONENT_LABEL,
+  LIST_ITEM_TITLE,
+} from "./constants";
 
 export default function Home() {
-  const DEFAULT_VALUE = "all";
-  const [selectedPlanet, setSelectedPlanet] = useState(DEFAULT_VALUE);
-
-  const handleChange: any = useCallback(
-    (e: any) => {
-      setSelectedPlanet(e.target.value);
-    },
-    [selectedPlanet]
+  const [selectedPlanet, setSelectedPlanet] = useState<string>(DEFAULT_VALUE);
+  const [visibleItems, setVisibleItems] = useState<number>(
+    DEFAULT_VISIBLE_ITEMS
   );
+
+  const handleChange = useCallback((e: ChangeEvent<HTMLSelectElement>) => {
+    setSelectedPlanet(e.target.value);
+  }, []);
+
+  const handleLoadMore = useCallback(() => {
+    setVisibleItems((prevVisibleItems) => prevVisibleItems + COLUMNS_QUANTITY);
+  }, []);
+
+  const handleClearAll = useCallback(() => {
+    setVisibleItems(NONE_VISIBLE_ITEMS);
+  }, []);
+
   return (
-    <main className="">
+    <main>
+      <Header title={HEADER_TITLE} body={HEADER_BODY} />
       <FilterComponent
-        label={"Filter By"}
+        label={FILTER_COMPONENT_LABEL}
         defaultValue={DEFAULT_VALUE}
         onChange={handleChange}
+        onClick={handleClearAll}
       />
       <ItemList
-        title={" All Characters"}
+        title={LIST_ITEM_TITLE}
         selectedPlanet={selectedPlanet}
         defaultValue={DEFAULT_VALUE}
+        visibleItems={visibleItems}
+        onClick={handleLoadMore}
       />
     </main>
   );
